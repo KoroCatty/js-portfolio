be882c63abcb7095149039bfbfca12d5bd9a413a
Kay <drumer19861018@gmail.com>
2022-03-09 19:44:57 +1100

    weather sectioin added2

--- src/javascripts/weatherApi.js
+++ src/javascripts/weatherApi.js
@@ -5,7 +5,7 @@ let weather = {
   fetchWeather: function (city) {
     // to obtain the metric(temp etc)
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q="
+      "https://api.openweathermap.org/data/2.5/weather?q="
       + city
       + "&units=metric&appid="
       + this.apiKey
